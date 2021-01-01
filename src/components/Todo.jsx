4ce81334279e78ed970e9efaf92033a9b138ff64
Vadim Curarari <vadimcurarari0@gmail.com>
2021-01-01 14:53:41 +0200

    New App

---
+++ src/components/Todo.jsx
@@ -0,0 +1,34 @@
+import { IconButton, CheckBox, ListItem, Typography } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import React from "react";
+
+function Todo({ todo, toggleComplete,removeTodo }) {
+    function handleCheckboxClick() {
+        toggleComplete(todo.id);
+    }
+
+    function handleRemoveClick() {
+       removeTodo(todo.id) 
+    }
+
+    return (
+        <ListItem style={{ display: "flex" }}>
+            <CheckBox 
+            checked={todo.completed}
+            onClick={handleCheckboxClick} />
+            <Typography
+            variant="body1"
+            style={{          
+                textDecoration: todo.completed ? "line=through" : null
+            }}
+            >
+                {todo.task}
+                </Typography>
+            <IconButton onClick={handleRemoveClick}>
+            <CloseIcon/>
+            </IconButton>
+        </ListItem>
+    );
+}
+
+export default Todo;
